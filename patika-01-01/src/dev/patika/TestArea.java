c40d7fc7b5e00c5ac086929519ecc9dba31b4c97
Koray Guney <koray.guney@hotmail.com>
2021-08-07 14:54:38 +0300

    Initial commit

---
+++ patika-01-01/src/dev/patika/TestArea.java
@@ -0,0 +1,16 @@
+package dev.patika;
+
+import java.text.DecimalFormat;
+
+public class TestArea {
+    public static void main(String[] args) {
+        Circle circle = new Circle(10.0, "Black");
+        System.out.println(circle.getColor() + " Circle area is " + circle.getArea());
+        System.out.println(circle.getColor() + " Circle area is " + new DecimalFormat("##.##").format(circle.getArea()));
+
+        Cylinder cylinder = new Cylinder(10.0, "Blue" , 15.0);
+        System.out.println(cylinder.getColor() + " Cylinder area is " + cylinder.getArea());
+        System.out.println(cylinder.getColor() + " Cylinder volume is " + cylinder.getVolume());
+
+    }
+}

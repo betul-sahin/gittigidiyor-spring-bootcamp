dac2588a4f111c82eebad814a0b159205bd4a8c9
Koray Guney <koray.guney@hotmail.com>
2021-08-20 21:02:41 +0300

    added brainstorm question

---
+++ patika-BrainStorm/src/dev/patika/StringBuilderGym.java
@@ -0,0 +1,18 @@
+package dev.patika;
+
+public class StringBuilderGym {
+    public static void main(String[] args) {
+
+        StringBuilder builder1 = new StringBuilder();
+        StringBuilder builder2 = new StringBuilder(0);
+        String hello = "hello";
+
+        hello.concat(" world");
+
+        builder1.append(hello);
+        builder2.append(builder1).append(" world");
+
+        System.out.println(builder1);
+        System.out.println(builder2);
+    }
+}

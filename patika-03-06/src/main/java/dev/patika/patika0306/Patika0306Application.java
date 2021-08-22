e9be6da2e73bd1bdbe29432e89c935bb7fd49ac9
Koray Guney <koray.guney@hotmail.com>
2021-08-22 15:05:47 +0300

    added 03-06 module

---
+++ patika-03-06/src/main/java/dev/patika/patika0306/Patika0306Application.java
@@ -0,0 +1,13 @@
+package dev.patika.patika0306;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Patika0306Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Patika0306Application.class, args);
+    }
+
+}

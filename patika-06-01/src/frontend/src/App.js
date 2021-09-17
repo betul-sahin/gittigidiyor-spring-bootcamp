c0fc9ab63aa23e0960d8135d11bcc2c6cbfe1d90
Koray Guney <koray.guney@hotmail.com>
2021-09-17 22:14:43 +0300

    updated patika-06-01

--- patika-06-01/src/frontend/src/App.js
+++ patika-06-01/src/frontend/src/App.js
@@ -223,7 +223,7 @@ function App() {
             </Content>
             <Footer style={{textAlign: 'center'}}>
                 <Image
-                    width={275}
+                    width={75}
                     src="https://app.patika.dev/patikaLogo.png"
                 />
                 <Divider>
@@ -231,7 +231,7 @@ function App() {
                         rel="noopener noreferrer"
                         target="_blank"
                         href="https://github.com/korayguney/gittigidiyor-spring-bootcamp">
-                        Click here to access Gittigidiyor Spring Bootcamp Github Repository
+                        Click here --> to access Gittigidiyor Spring Bootcamp Github Repository
                     </a>
                 </Divider>
             </Footer>
